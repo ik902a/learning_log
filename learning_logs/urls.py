28ec5c35df872e0f9ff6dd7169acc68b82895295
Ihar Klepcha <Ihar_Klepcha@epam.com>
2025-02-11 15:36:22 +0300

    Web. lls

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -1,3 +1,5 @@
+"""Определяет схемы URL для learning_logs."""
+
 from django.urls import path
 from . import views
 
@@ -5,4 +7,5 @@ app_name = 'learning_logs'
 urlpatterns = [
     path('', views.index, name='index'),
     path('topics/', views.topics, name='topics'),
+    path('topics/<int:topic_id>/', views.topic, name='topic'),
 ]
